df5c6eb4232ac046d64562f8e27de566fd57284b
Dylan Fraser <86073348+SimplyPickles@users.noreply.github.com>
2026-05-13 21:25:58 -0400

    Remove ProgBlur import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Header from "./components/Header";
 import Background from "./components/Background";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import ProgBlur from "./components/ProgBlur";
 
 function App(): ReactElement {
   const heroRef = useRef<HTMLDivElement>(null);
